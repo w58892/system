8a5bd0a48591004bb54f02e2189e7350de2ffb34
Damian Oliwa <50053172+w58892@users.noreply.github.com>
2020-09-21 19:11:07 +0200

    main page

---
+++ reset.php
@@ -0,0 +1,13 @@
+<?php
+session_start();
+
+
+
+if(isset($_POST['reset'])){
+    $_SESSION['choosed']="0";
+}elseif(isset($_POST['back'])){
+    $_SESSION['choosed'] = substr($_SESSION['choosed'], 0, -1);
+}
+echo $_SESSION['choosed'];
+
+?>
